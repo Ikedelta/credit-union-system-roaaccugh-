56fe0927ac9ce88904bffd2f53c4d0a3b4d56c44
Ikedelta <asantedarkoisaac360@gmail.com>
2026-08-14 23:53:05 +0000

    Pre Production

--- apps/website/src/pages/NewsDetail.tsx
+++ apps/website/src/pages/NewsDetail.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { useCMS } from '../context/CMSContext';
 import { PageHeader } from '../components/PageHeader';
